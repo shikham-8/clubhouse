703fa58f30b9b88f594f921f1cf9a4714da2a0ce
shikham-8 <shikhamody1@gmail.com>
2020-03-01 13:06:06 -0800

    backup branch

---
+++ clubhouse/clubhouse/clubProfile.swift
@@ -0,0 +1,21 @@
+//
+//  clubProfile.swift
+//  clubhouse
+//
+//  Created by shikha on 3/1/20.
+//  Copyright © 2020 Samritha Nagesh. All rights reserved.
+//
+
+import SwiftUI
+
+struct clubProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct clubProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        clubProfile()
+    }
+}
