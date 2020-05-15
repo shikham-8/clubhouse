0f30b2d68cbc29a8a776e34c8a77a85873dcd031
Samritha Nagesh <sam.nag03@gmail.com>
2020-05-14 19:50:14 -0700

    adding student view of club profile

--- clubhouse/clubhouse/SceneDelegate.swift
+++ clubhouse/clubhouse/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         // Create the SwiftUI view that provides the window contents.
-        let contentView = HomePage()
+        let contentView = studentClubProfile()
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
@@ -58,4 +58,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
 
+}
+
+struct SceneDelegate_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
 }
