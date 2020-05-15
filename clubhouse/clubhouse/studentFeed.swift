c98514576687151f99798e995417a482c0d33120
Samritha Nagesh <sam.nag03@gmail.com>
2020-05-15 14:35:49 -0700

    modified studentfeed

--- clubhouse/clubhouse/studentFeed.swift
+++ clubhouse/clubhouse/studentFeed.swift
@@ -54,70 +54,72 @@ struct feedFooter: View{
             }.edgesIgnoringSafeArea(.bottom)
         }
 }
-struct studentFeed: View {
-    @State var eventTiles = ["club"]
-    let db = Firestore.firestore()
-
-    struct eventTile: View {
-        @State var name = "Meeting 1"
-        @State var club = "UCLA Club"
-        @State var desc = "Enter a description for the event"
-        @State var date = "Friday, Jan 1, 2021"
-        @State var loc = "UCLA Royce Hall"
-        
-        //commented init only for testing purposes
-        /*init(name1 : String, club1: String, desc1: String, date1: String, loc1: String) {
-            name = name1
-            club = club1
-            desc = desc1
-            date = date1
-            loc = loc1
-        } */
-        
-        var body: some View {
-            ZStack() {
-            Rectangle()
-                .fill(Color.white)
-                .frame(width: 425, height: 150)
-                HStack() {
-                    circleImage()
-                        .frame(width: 30, height: 30)
-                    VStack(alignment: .leading) {
-                        Text(name)
-                            .font(Font.custom("Montserrat-Medium", size: 20))
-                        Text(self.club)
-                            .foregroundColor(Color.gray)
-                        Text(self.desc).foregroundColor(Color.gray)
-                        Text(self.date).foregroundColor(Color.gray)
-                        Text(self.loc).foregroundColor(Color.gray)
-                    }.padding(.horizontal)
-                    
-                    Spacer()
-                }.padding()
-            }
-        }
-    }
+struct eventTile: View {
+    @State var name = "Meeting 1"
+    @State var club = "UCLA Club"
+    @State var desc = "Enter a description for the event"
+    @State var date = "Friday, Jan 1, 2021"
+    @State var loc = "UCLA Royce Hall"
+    
+    //commented init only for testing purposes
+    /*init(name1 : String, club1: String, desc1: String, date1: String, loc1: String) {
+        name = name1
+        club = club1
+        desc = desc1
+        date = date1
+        loc = loc1
+    } */
+    
     var body: some View {
         ZStack() {
-        ScrollView {
-            VStack(spacing: 10) {
-                feedHeader()
-                if(eventTiles.isEmpty)
-                {
-                    Text("Save Clubs to Start Viewing Events")
-                        .font(Font.custom("Roboto-normal", size: 24))
-                        .foregroundColor(Color.black)
-                        .padding(.top, 300)
+        Rectangle()
+            .fill(Color.white)
+            .frame(width: 425, height: 150)
+            HStack() {
+                circleImage()
+                    .frame(width: 30, height: 30)
+                VStack(alignment: .leading) {
+                    Text(name)
+                        .font(Font.custom("Montserrat-Medium", size: 20))
+                    Text(self.club)
+                        .foregroundColor(Color.gray)
+                    Text(self.desc).foregroundColor(Color.gray)
+                    Text(self.date).foregroundColor(Color.gray)
+                    Text(self.loc).foregroundColor(Color.gray)
+                }.padding(.horizontal)
+                
+                Spacer()
+            }.padding()
+        }
+    }
+}
+struct studentFeed: View {
+    @State var eventTiles = ["club"]
+    @State var currentView = "calendar"
+    let db = Firestore.firestore()
+
+    
+    var body: some View {
+        ZStack() {
+            ScrollView {
+                VStack(spacing:10) {
+                    feedHeader()
+                    if(eventTiles.isEmpty)
+                    {
+                        Text("Save Clubs to Start Viewing Events")
+                            .font(Font.custom("Roboto-normal", size: 24))
+                            .foregroundColor(Color.black)
+                            .padding(.top, 300)
+                    }
+                    else{
+                        //display event tiles
+                        eventTile()
+                        eventTile()
+                    }
+                
                 }
-                else{
-                    //display event tiles
-                    eventTile()
-                    eventTile()
-                }
-            
-            }
-        }.background(Color.CustomLightGray)
-         feedFooter()
+            }.background(Color.CustomLightGray)
+            feedFooter()
         }
     }
 }
